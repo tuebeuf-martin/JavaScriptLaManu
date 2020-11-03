dc778e6fb2d53312fd2fbd66d95294ac826156b4
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-11-03 11:34:34 +0100

    Ajout de l'exercice 5

---
+++ Exercice5/assets/js/main.js
@@ -0,0 +1,5 @@
+function sendData(){
+    var number1 = document.getElementById("1stnumber").value;
+    var number2 = document.getElementById("2ndnumber").value;
+    alert ("Le resultat : "+number1%number2)
+}
