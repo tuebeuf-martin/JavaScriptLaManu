bcac66ec27a62ab89242e716381cce7a2c7e9d9e
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-11-03 12:16:10 +0100

    Ajout de l'exercice 6

---
+++ Exercice6/assets/js/main.js
@@ -0,0 +1,8 @@
+function sendData(){
+    var number1 = document.getElementById("1stnumber").value;
+    var number2 = document.getElementById("2ndnumber").value;
+
+    resultat = (number1 * 2)+5(50)-number2+1769;
+
+    alert ("Le resultat : "+ resultat)
+}
