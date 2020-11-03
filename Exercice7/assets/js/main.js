b0559b66952ea06d0e13ab7f4dc6bb6676c761b2
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-11-03 16:25:20 +0100

    Ajout de l'exo 7

---
+++ Exercice7/assets/js/main.js
@@ -0,0 +1,11 @@
+function divideThem(){
+   var age = document.getElementById("age").value;
+    if (age >= 18) {
+      alert("Vous êtes majeur ! Stylé");
+}   else {
+      alert("vous êtes mineur ! Rentre chez toi");
+}
+}
+ 
+
+    
---
+++ Exercice7/main.js
@@ -0,0 +1,10 @@
+
+}function testNum(18) {
+    let result;
+    if (age > 18 ) {
+      result = "Vous étes majeur";
+    } else {
+      result = "Vous n'étes pas majeur";
+    }
+    return result;
+  }
