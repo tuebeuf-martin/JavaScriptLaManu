e55e0c320aff20391edc83bdaa420d2ad6d40bf8
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-11-06 15:01:56 +0100

    Ajout de l'exercice 4 de la partie 2

---
+++ ExoJquerry/Exercice Partie 2 n°4/assets/js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $('#buttonblue').click(function(){
+        $('p').css("color","blue");
+    });   
+    $('#buttongreen').click(function(){
+        $('p').css("color","green");
+    }); 
+    
+});
+    
